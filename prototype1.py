e383ec4c2ca3ee36c18a5f8ddad831de20525131
joellenroberts <jor@marketingprofs.com>
2025-06-30 23:02:05 -0500

    Updated imports to reflect simpler game play

--- prototype1.py
+++ prototype1.py
@@ -1,29 +1,17 @@
 
 # 
-# IMPORTS -- delete documentation links prior to live implementation
+# IMPORTS
 #
 
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user 
-from flask_wtf import FlaskForm
-from flask_socketio import SocketIO, emit, join_room, leave_room 
-    ###  documentation: https://flask.palletsprojects.com/en/stable/
-
-from jinja2 import Template
-    ###  documentation: https://jinja.palletsprojects.com/en/stable/
+from flask import Flask, render_template, request, redirect, url_for, session, flash 
+import html
+import random  
+import sys
+import requests
   
-import os # for phase 2 upgrade from sqlite (initial prototype) to PostgreSQL (needed for 10+ simultaeous players)
-import json
-from datetime import datetime 
-from random import randint
-
 
 # 
 # APP SET UP
 #
 
 app = Flask(__name__)
-
-
-
